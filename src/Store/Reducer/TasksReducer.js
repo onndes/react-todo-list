32125b18f33e36a4fdea3535d751762e9dd53cee
onndes <vladvyljotnikov@gmail.com>
2021-10-23 15:47:47 +0300

    added func. in TasksReducer (delete and rename tasks)
    and fix

--- src/Store/Reducer/TasksReducer.js
+++ src/Store/Reducer/TasksReducer.js
@@ -4,6 +4,8 @@ import { todoAPI } from "./../../API/API";
 const SET_TASKS_LIST = "TasksReducer/SET_TASKS_LIST";
 const SET_SELECTED_TODO_ID = "TasksReducer/SET_SELECTED_TODO_ID";
 const SET_ADD_TASK = "TasksReducer/SET_ADD_TASK";
+const DELETE_TASKS_LIST = "TasksReducer/DELETE_TASKS_LIST";
+const RENAME_TASKS_LIST = "TasksReducer/RENAME_TASKS_LIST";
 
 const initialState = {
     tasksList: null,
@@ -25,7 +27,25 @@ const TasksReducer = (state = initialState, action) => {
         case SET_ADD_TASK:
             return {
                 ...state,
-                tasksList: [...state.tasksList, action.newTask],
+                tasksList: [action.newTask, ...state.tasksList],
+            };
+        case DELETE_TASKS_LIST:
+            return {
+                ...state,
+                tasksList: state.tasksList.filter((item) => item.id !== action.taskId),
+            };
+        case RENAME_TASKS_LIST:
+            return {
+                ...state,
+                tasksList: state.tasksList.map((item) => {
+                    if (item.id === action.taskId) {
+                        return {
+                            ...item,
+                            title: action.newTitle,
+                        };
+                    }
+                    return item;
+                }),
             };
         default:
             return state;
@@ -35,25 +55,45 @@ const TasksReducer = (state = initialState, action) => {
 export const setTasksList = (tasks) => {
     return { type: SET_TASKS_LIST, tasks };
 };
-export const setSelectedToDoID = (id) => {
+export const setSelectedToDoId = (id) => {
     return { type: SET_SELECTED_TODO_ID, id };
 };
 export const setAddTask = (newTask) => {
     return { type: SET_ADD_TASK, newTask };
 };
+const deleteTaskAC = (taskId) => {
+    return { type: DELETE_TASKS_LIST, taskId };
+};
+const renameTaskAC = (taskId, newTitle) => {
+    return { type: RENAME_TASKS_LIST, taskId, newTitle };
+};
 
 export const getTasks = (order) => async (dispatch) => {
     const todoLists = await todoAPI.getTodoLists();
     const selectedTodo = todoLists.filter((item) => Math.abs(item.order) + 1 === +order);
-    dispatch(setSelectedToDoID(selectedTodo[0].id));
     const resault = await taksksAPI.getTasks(selectedTodo[0].id);
+    dispatch(setSelectedToDoId(selectedTodo[0].id));
     dispatch(setTasksList(resault.items));
 };
 
 export const addTask = (taskText) => async (dispatch, getState) => {
     const resault = await taksksAPI.createTask(getState().tasks.selectedTodoID, taskText);
-    if (resault.resaultCode === 0) {
-        dispatch(setAddTask(taskText));
+    console.log(resault);
+    console.log(resault.resultCode);
+    if (resault.resultCode === 0) {
+        dispatch(setAddTask(resault.data.item));
+    }
+};
+export const deleteTask = (taskId) => async (dispatch, getState) => {
+    const resault = await taksksAPI.deleteTaskList(getState().tasks.selectedTodoID, taskId);
+    if (resault.data.resultCode === 0) {
+        dispatch(deleteTaskAC(taskId));
+    }
+};
+export const renameTask = (taskId, title) => async (dispatch, getState) => {
+    const resault = await taksksAPI.renameTaskList(getState().tasks.selectedTodoID, taskId, title);
+    if (resault.data.resultCode === 0) {
+        dispatch(renameTaskAC(taskId, title));
     }
 };
 
