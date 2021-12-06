51f4659c8c72aa3bb7f0493598708b3998dd459d
onndes <vladvyljotnikov@gmail.com>
2021-12-06 14:31:37 +0200

    fixed bug with API KEY danger

--- src/API/API.js
+++ src/API/API.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.1/",
     withCredentials: true,
     headers: {
-        "API-KEY": "d496f6cf-9a97-46d6-928f-3e138a218850",
+        "API-KEY": process.env.REACT_APP_WEATHER_API_KEY,
     },
 });
 
